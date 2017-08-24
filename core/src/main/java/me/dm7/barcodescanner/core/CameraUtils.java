b6b6774887dc6ba076c7338c3248dce4965cd6b0
IzabellaConigliaro <izabella.conigliaro@hotmart.com.br>
2017-08-24 07:41:37 -0300

    workaround for isFlashSupported throwing a RuntimeException.
    https://github.com/dm77/barcodescanner/issues/235

--- core/src/main/java/me/dm7/barcodescanner/core/CameraUtils.java
+++ core/src/main/java/me/dm7/barcodescanner/core/CameraUtils.java
@@ -42,22 +42,31 @@ public class CameraUtils {
     }
 
     public static boolean isFlashSupported(Camera camera) {
-        /* Credits: Top answer at http://stackoverflow.com/a/19599365/868173 */
+		/* Credits: Top answer at http://stackoverflow.com/a/19599365/868173 */
+
+        boolean retVal = true;
+
         if (camera != null) {
-            Camera.Parameters parameters = camera.getParameters();
+            try {
+                Camera.Parameters parameters = camera.getParameters();
 
-            if (parameters.getFlashMode() == null) {
-                return false;
-            }
+                if (parameters.getFlashMode() == null) {
+                    retVal = false;
+                }
 
-            List<String> supportedFlashModes = parameters.getSupportedFlashModes();
-            if (supportedFlashModes == null || supportedFlashModes.isEmpty() || supportedFlashModes.size() == 1 && supportedFlashModes.get(0).equals(Camera.Parameters.FLASH_MODE_OFF)) {
-                return false;
+                List<String> supportedFlashModes = parameters.getSupportedFlashModes();
+                if (supportedFlashModes == null || supportedFlashModes.isEmpty() || supportedFlashModes.size() == 1
+                        && supportedFlashModes.get(0).equals(Camera.Parameters.FLASH_MODE_OFF)) {
+                    retVal = false;
+                }
+            } catch (RuntimeException e) {
+                // may be: "Fatal Exception: java.lang.RuntimeException: getParameters failed (empty parameters)"
+                retVal = false;
             }
         } else {
-            return false;
+            retVal = false;
         }
 
-        return true;
+        return retVal;
     }
 }
